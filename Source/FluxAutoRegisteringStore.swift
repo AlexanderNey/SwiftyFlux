5655a16d5d1cc467d0f1070e1e30bb26348acbaa
Alexander Ney <alexander.ney@me.com>
2015-11-19 14:48:19 +0000

    refactored Store - is now public - separated concrete Store implementation from abstract declarations

---
+++ Source/FluxAutoRegisteringStore.swift
@@ -0,0 +1,59 @@
+//
+//  FluxAutoRegisteringStore.swift
+//  SwityFlux
+//
+//  Created by Alexander Ney on 19/11/2015.
+//  Copyright © 2015 Alexander Ney. All rights reserved.
+//
+
+import Foundation
+
+
+class FluxAutoRegisteringStore : Store {
+    
+    let dispatcher: ActionDispatcher
+    private var observer = [StoreObserver]()
+    var latestUpdate: NSDate?
+    
+    // MARK: Store
+    
+    required init(dispatcher: ActionDispatcher) {
+        self.dispatcher = dispatcher
+        self.dispatcher.registerStore(self)
+    }
+    
+    deinit {
+        self.dispatcher.unregisterStore(self)
+    }
+    
+    func addObserver(observer: StoreObserver) {
+        if !self.observer.contains({ $0 === observer }) {
+            self.observer.append(observer)
+            observer.storeDidChange(self)
+        }
+    }
+    
+    func removeObserver(observer: StoreObserver) {
+        self.observer = self.observer.filter { $0 !== observer }
+    }
+    
+    
+    // MARK: ActionSubscriber
+    
+    func digestAction(action: Action) -> Bool {
+        return false
+    }
+    
+    
+    // MARK: Notify Observer
+    
+    func storeChangeNotifyObserver() {
+        self.latestUpdate = NSDate()
+        dispatch_async(dispatch_get_main_queue()) {
+            for observer in self.observer {
+                observer.storeDidChange(self)
+            }
+        }
+    }
+    
+}
--- Source/Store.swift
+++ Source/Store.swift
@@ -8,63 +8,13 @@
 
 import Foundation
 
-protocol Store: class, ActionSubscriber {
+public protocol Store: class, ActionSubscriber {
     var latestUpdate: NSDate? { get }
-    init(dispatcher: ActionDispatcher)
     func addObserver(observer: StoreObserver)
     func removeObserver(observer: StoreObserver)
     func storeChangeNotifyObserver()
 }
 
-protocol StoreObserver: class {
-    func storeDidChange(store: Store)
-}
-
-class BaseStore : Store {
-    
-    let dispatcher: ActionDispatcher
-    private var observer = [StoreObserver]()
-    var latestUpdate: NSDate?
-    
-    // MARK: Store
-    
-    required init(dispatcher: ActionDispatcher) {
-        self.dispatcher = dispatcher
-        self.dispatcher.registerSubscriber(self)
-    }
-    
-    deinit {
-        self.dispatcher.unregisterSubscriber(self)
-    }
-    
-    func addObserver(observer: StoreObserver) {
-        if !self.observer.contains({ $0 === observer }) {
-            self.observer.append(observer)
-            observer.storeDidChange(self)
-        }
-    }
-    
-    func removeObserver(observer: StoreObserver) {
-        self.observer = self.observer.filter { $0 !== observer }
-    }
-    
-    
-    // MARK: ActionSubscriber
-    
-    func digestAction(action: Action) -> Bool {
-        return false
-    }
-    
-    
-    // MARK: Notify Observer
-    
-    func storeChangeNotifyObserver() {
-        self.latestUpdate = NSDate()
-        onMainThread {
-            for observer in self.observer {
-                observer.storeDidChange(self)
-            }
-        }
-    }
-    
+public protocol StoreObserver: class {
+    func storeDidChange<S: Store>(store: S)
 }
